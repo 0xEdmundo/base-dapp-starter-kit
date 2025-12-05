c0f23d328c4b5343ee88cdfe1e94b80c13bd2fe8
0xEdmundo <121467866+0xEdmundo@users.noreply.github.com>
2025-12-06 01:31:56 +0300

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,35 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    base: {
+      url: "https://mainnet.base.org",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 8453,
+    },
+    baseSepolia: {
+      url: "https://sepolia.base.org",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 84532,
+    }
+  },
+  etherscan: {
+    apiKey: {
+     base: process.env.ETHERSCAN_API_KEY,
+     baseSepolia: process.env.ETHERSCAN_API_KEY
+    },
+    customChains: [
+      {
+        network: "base",
+        chainId: 8453,
+        urls: {
+          apiURL: "https://api.basescan.org/api",
+          browserURL: "https://basescan.org"
+        }
+      }
+    ]
+  }
+};
